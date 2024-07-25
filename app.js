65432f02702040f121ef93dbac8d3eddb7e56682
Bunma93 <bunma93@gmail.com>
2024-07-25 15:14:37 +0700

    Js-arr47

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // write code here
+let arr = ['React', 'Vue', 'Angular'];
+let sorted = copySorted(arr);
+
+function copySorted(arr) {
+    return arr2 = arr.slice().sort();
+};
+
+console.log(sorted); // Angular, React, Vue
+console.log(arr); // React, Vue, Angular (no changes)
